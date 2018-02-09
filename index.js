27c7d7ce174bb33c9ccdaa5c5b49c5ab6a42afdf
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-09 21:19:08 +1300

    rewriting interms of epidemic-broadcast-trees@4

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict'
 var Obv = require('obv')
 var pull = require('pull-stream')
-var pContDuplex = require('pull-cont/duplex')
-var EBTStream = require('epidemic-broadcast-trees')
-var Bounce = require('epidemic-broadcast-trees/bounce')
+//var pContDuplex = require('pull-cont/duplex')
+var EBT = require('epidemic-broadcast-trees')
 var path = require('path')
-var Follows = require('./follows')
+var toPull = require('push-stream-to-pull-stream')
 
 var Store = require('lossy-store')
 var toUrlFriendly = require('base64-url').escape
@@ -44,98 +43,60 @@ exports.init = function (sbot, config) {
   config.replicate = config.replicate || {}
   config.replicate.fallback = true
 
-  var status = {}
-  var clock = require('./clock')(sbot)
 
-  var dir = config.path ? path.join(config.path, 'ebt') : null
-  var store = Store(dir, null, toUrlFriendly)
-  var follows = Follows(
-    store,
-    clock, status
-  )
+  var ebt = EBT({
+    id: sbot.id,
+    getClock: function (id, cb) {
+      cb(null, {})
+    },
+    getAt: function (pair, cb) {
+      sbot.getAtSequence([pair.id, pair.sequence], function (err, data) {
+        cb(err, data ? data.value : null)
+      })
+    },
+    append: function (msg, cb) {
+      sbot.add(msg, cb)
+    },
+  })
+
+  sbot.getVectorClock(function (err, clock) {
+    ebt.state.clock = clock
+    ebt.update()
+  })
+
+  sbot.post(function (msg) {
+    ebt.onAppend(msg.value)
+  })
+
+  var status = {}
 
   //HACK: patch calls to replicate.request into ebt, too.
   hook(sbot.replicate.request, function (fn, args) {
-    follows.request(args[0], args[1])
+    ebt.request(args[0], args[1])
     return fn.apply(this, args)
   })
 
-  //this should be always up to date...
-
-  sbot.post(function (data) {
-    follows.onAppend(data.value)
-  })
-
-  var createStream = EBTStream(
-    function get (id, seq, cb) {
-      sbot.getAtSequence([id, seq], function (err, data) {
-        cb(null, data && data.value || data)
-      })
-    },
-    function append (msg, cb) {
-      ;(sbot.queue || sbot.add)(msg, function (err) {
-        if(!err)
-          follows.onAppend(msg)
-//        if(err) throw err
-        cb()
-      })
-    }
-  )
-
   var ts = Date.now(), start = Date.now()
 
-  function replicate (other, opts, callback) {
-    if('function' === typeof opts)
-      callback = opts, opts = null
-    if(!opts || opts.version !== 2)
-      throw new Error('expected ebt.replicate({version: 2})')
-
-    return follows.add(other, createStream({
-      onChange: Bounce(function () {
-        //TODO: log progress in some way, here
-        //maybe save progress to a object, per peer.
-        follows.update(other)
-      }, 200),
-      onRequest: function (id, seq) {
-        follows.onRequest(id, seq, other)
-      },
-      //TODO: if we switch this stream into main mode,
-      //request non-send for other streams.
-      onSwitch: function (id, seq) {
-        //...
-        console.log("SWITCH", id, seq)
-      }
-    },  function (err) {
-      //remember their clock, so we can skip requests next time.
-      follows.update(other)
-      callback && callback(err)
-    }))
-  }
-
-  hook(sbot.status, function (fn) {
-    var _status = fn(), feeds = 0
-    _status.ebt = follows.status()
-    return _status
-  })
-
-  hook(sbot.progress, function (fn) {
-    var prog = fn()
-    var p = follows.progress()
-    if(p.target) prog.ebt = p
-    return prog
-  })
+//  hook(sbot.status, function (fn) {
+//    var _status = fn(), feeds = 0
+//    _status.ebt = ebt.status()
+//    return _status
+//  })
+//
+//  hook(sbot.progress, function (fn) {
+//    var prog = fn()
+//    var p = follows.progress()
+//    if(p.target) prog.ebt = p
+//    return prog
+//  })
 
   sbot.on('rpc:connect', function (rpc, isClient) {
     if(isClient) {
       var opts = {version: 2}
-      var a = replicate(rpc.id, opts, function (err) {
-        if(!rpc.closed) {
-          console.log('EBT failed, fallback to legacy', err)
-          rpc._emit('fallback:replicate', err) //trigger legacy replication
-        }
-      })
+      var a = toPull.duplex(ebt.createStream(rpc.id))
       var b = rpc.ebt.replicate(opts, function (err) {
-        console.log('replication ended:', rpc.id, err && err.stack)
+        rpc._emit('fallback:replicate', err)
       })
 
       pull(a, b, a)
@@ -143,16 +104,16 @@ exports.init = function (sbot, config) {
   })
 
   return {
-    replicate: function (opts, cb) {
-      return replicate(this.id, opts, cb)
+    replicate: function (opts) {
+      if(opts.version !== 2)
+        throw new Error('expected ebt.replicate({version: 2})')
+      return toPull.duplex(ebt.createStream(this.id))
     },
     _state: function () {
-      var streams = {}
-      for(var k in follows.streams)
-        streams[k] = follows.streams[k].states
-      return {streams: streams, follows: follows.follows}
+      return ebt.state
     },
     _dump: require('./debug/local')(sbot) //just for performance testing. not public api
   }
 }
+
 
