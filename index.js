e568f7e608c68d0a5be2e724e7efc92cd2ab47cc
Anders Rune Jensen <arj03@protonmail.ch>
2018-07-06 09:05:16 +0200

    Handle live data. Thanks @dominic

--- index.js
+++ index.js
@@ -119,22 +119,32 @@ exports.init = function (sbot, config) {
 
   //wait till next tick, incase ssb-friends hasn't been installed yet.
   setImmediate(function () {
-    if(sbot.friends)
+    if(sbot.friends) {
+      function handleBlockUnlock(from, to, value)
+      {
+        if (value === false)
+          ebt.block(from, to, true)
+        else if (ebt.state.blocks[from] && ebt.state.blocks[from][to])
+          ebt.block(from, to, false)
+      }
+
       pull(
         sbot.friends.stream({live: true}),
         pull.drain(function (contacts) {
           if(!contacts) return
-          for (var from in contacts) {
-            var relations = contacts[from]
-            for (var to in relations) {
-              if(relations[to] === false)
-                ebt.block(from, to, true)
-              else if (ebt.state.blocks[from] && ebt.state.blocks[from][to])
-                ebt.block(from, to, false)
+
+          if (contacts.from) { // live data
+            handleBlockUnlock(contacts.from, contacts.to, contacts.value)
+          } else { // initial data
+            for (var from in contacts) {
+              var relations = contacts[from]
+              for (var to in relations)
+                handleBlockUnlock(from, to, relations[to])
             }
           }
         })
       )
+    }
   })
 
   return {
