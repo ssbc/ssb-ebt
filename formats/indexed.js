e295374aa5b4d8bc5f460135d5abec6dfe761711
Andre Staltz <andre@staltz.com>
2022-09-07 17:52:05 +0300

    preliminary work on indexed-v1 feed format

--- formats/indexed.js
+++ formats/indexed.js
@@ -1,56 +1,31 @@
-const pify = require('promisify-4loc')
-const ref = require('ssb-ref')
-const { QL0 } = require('ssb-subset-ql')
+const indexed = require('../indexed-feed-format')
+const ebtFormatFrom = require('./base')
 
 module.exports = {
-  name: 'indexed',
-  prepareForIsFeed(sbot, feedId, cb) {
-    sbot.metafeeds.ensureLoaded(feedId, cb)
-  },
-  isFeed(sbot, author) {
-    const info = sbot.metafeeds.findByIdSync(author)
-    return info && info.feedpurpose === 'index'
-  },
+  ...ebtFormatFrom(indexed),
+
   appendMsg(sbot, msgTuple, cb) {
     const [msgVal, payload] = msgTuple
-    sbot.db.addTransaction([msgVal], [payload], cb)
+    sbot.db.add(msgVal, this.appendOpts, (err) => {
+      if (err) {
+        return cb(err)
+      }
+      sbot.db.addOOO(payload, { feedFormat: 'classic' }, (err) => {
+        cb(err)
+      })
+    })
+    // FIXME: addTransaction should support heterogenous feed formats
+    // FIXME: indexed-feed-format needs to support validateBatch/BatchOOO
+    // sbot.db.addTransaction([msgVal], [payload], cb)
   },
+
   getAtSequence(sbot, pair, cb) {
     sbot.getAtSequence([pair.id, pair.sequence], (err, msg) => {
       if (err) return cb(err)
-
-      module.exports.convertMsg(sbot, msg.value, cb)
+      sbot.db.get(msg.value.content.indexed.key, (err, indexedMsgVal) => {
+        if (err) return cb(err)
+        cb(null, [msg.value, indexedMsgVal])
+      })
     })
-  },
-  convertMsg(sbot, msgVal, cb) {
-    const { sequence } = msgVal.content.indexed
-    const authorInfo = sbot.metafeeds.findByIdSync(msgVal.author)
-    if (!authorInfo) return cb(new Error('Unknown author:' + msgVal.author))
-    const { author } = QL0.parse(authorInfo.metadata.query)
-    sbot.getAtSequence([author, sequence], (err, indexedMsg) => {
-      if (err) return cb(err)
-
-      cb(null, [msgVal, indexedMsg.value])
-    })
-  },
-  isReady(sbot) {
-    return pify(sbot.metafeeds.loadState)()
-  },
-  isMsg(msgTuple) {
-    if (Array.isArray(msgTuple) && msgTuple.length === 2) {
-      const [msgVal, payload] = msgTuple
-      return (
-        Number.isInteger(msgVal.sequence) &&
-        msgVal.sequence > 0 &&
-        ref.isFeed(msgVal.author) &&
-        msgVal.content
-      )
-    } else return false
-  },
-  getMsgAuthor(msgTuple) {
-    return msgTuple[0].author
-  },
-  getMsgSequence(msgTuple) {
-    return msgTuple[0].sequence
   },
 }
